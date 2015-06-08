ecad20ab99f7a1235f329da990fd5b5b5dc66a8e
amunozhernan <amunozhernan.externe@pagesjaunes.fr>
2015-06-08 09:36:20 +0200

    Suppression des solutions pour branch master

--- src/test/java/org/jduchess/handson/streams/Exercise1.java
+++ src/test/java/org/jduchess/handson/streams/Exercise1.java
@@ -1,8 +1,10 @@
 package org.jduchess.handson.streams;
 
+import org.assertj.core.util.Lists;
 import org.jduchess.handson.Dish;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,41 +15,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 //Intermediate & terminal Operations
 public class Exercise1 extends TestBase {
 
-    //filter
     @Test
     public void should_find_out_all_vegetarian_dishes(){
-        List<Dish> vegetarianDishes = menu.stream().filter(Dish::isVegetarian).collect(toList());
+        List<Dish> vegetarianDishes = Lists.newArrayList();
+
+        // ADD CODE HERE
 
         assertThat(vegetarianDishes).isNotEmpty();
         assertThat(vegetarianDishes).hasSize(4);
 
     }
 
-    //map
     @Test
-    public void should_collect_all_dishes_name(){
-        List<String> names = menu.stream().map(Dish::getName).collect(toList());
+    public void should_collect_all_dishes_names(){
+        List<String> names = Lists.newArrayList();
+
+        // ADD CODE HERE
 
         assertThat(names).isNotEmpty();
         assertThat(names).hasSize(menu.size());
 
     }
 
-    //distinct
     @Test
-    public void should_find_out_distinct_types_of_dishes(){
-        long count = menu.stream().map(Dish::getType).distinct().count();
+    public void should_find_out_how_many_distinct_types_of_dishes(){
+        long count = 0;
+
+        // ADD CODE HERE
 
         assertThat(count).isEqualTo(3);
 
     }
 
-    //limit
-    //How would you use streams to filter the first two meat dishes?
     @Test
-    public void should_get_the_any_fish_dish(){
+    public void should_get_any_fish_dish(){
 
-        Optional<Dish> anyFishDish = menu.stream().filter(d -> Dish.Type.FISH.equals(d.getType())).findAny();
+        Optional<Dish> anyFishDish = Optional.empty();
+
+        // ADD CODE HERE
 
         assertThat(anyFishDish.isPresent()).isTrue();
         assertThat(anyFishDish.get().getType()).isEqualTo(Dish.Type.FISH);
@@ -55,43 +60,50 @@ public class Exercise1 extends TestBase {
 
     }
 
-    //map
-    // to find out the length of the name of each dish
     @Test
-    public void should_find_out_legth_of_the_name_of_each_dish(){
-        List<Integer> dishNameLengths = menu.stream() .map(Dish::getName)
-                .map(String::length)
-                .collect(toList());
+    public void should_find_out_the_legth_of_the_name_of_each_dish(){
+        List<Integer> dishNameLengths = Lists.newArrayList();
+
+        // ADD CODE HERE
+
+        assertThat(dishNameLengths).isNotEmpty();
+
 
     }
 
-    //flatmap?? pags 125
+    @Test
+    public void should_count_the_number_of_characteres_of_any_dish(){
+        List<String> uniqueCharacters = Lists.newArrayList();
 
-    //Filtering a menu using a stream to find out three high-calorie dish names -
+        // ADD CODE HERE
+
+        assertThat(uniqueCharacters).isNotEmpty();
+    }
+
     @Test
     public void should_find_out_three_high_calorie_dish_names(){
-        List<String> highCalorieDishes = menu.stream().filter(d -> d.getCalories() > 300)
-                                .map(Dish::getName)
-                                .limit(3)
-                                .collect(toList());
+        List<String> highCalorieDishes = Lists.newArrayList();
+
+        // ADD CODE HERE
 
         assertThat(highCalorieDishes).isNotEmpty();
         assertThat(highCalorieDishes).hasSize(3);
     }
 
-    //reduce
     @Test
     public void should_count_number_of_dishes_using_reduce(){
-        int count = menu.stream() .map(d -> 1)
-                .reduce(0, (a, b) -> a + b);
+        int count = 0;
+
+        // ADD CODE HERE
+
         assertThat(count).isEqualTo(9);
     }
 
-    //reduce
     @Test
-    public void should_count_number_of_calories_in_menu(){
-        int calories = menu.stream() .map(Dish::getCalories)
-                .reduce(0, Integer::sum);
+    public void should_count_number_of_calories_in_menu_using_reduce(){
+        int calories =0;
+
+        // ADD CODE HERE
 
         assertThat(calories).isEqualTo(4200);
     }
