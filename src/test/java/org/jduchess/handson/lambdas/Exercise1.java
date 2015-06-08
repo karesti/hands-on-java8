3ab79a7827d7690c4036e7e4d0b05d3b38e5c3e7
sandrine.cariteau <sandrine.cariteau-ext@alstom.com>
2015-06-08 23:26:30 +0200

    adding method reference to tests for lambdas

--- src/test/java/org/jduchess/handson/lambdas/Exercise1.java
+++ src/test/java/org/jduchess/handson/lambdas/Exercise1.java
@@ -5,6 +5,7 @@ import org.jduchess.handson.TestBase;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -13,6 +14,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 //Intermediate & terminal Operations
 public class Exercise1 extends TestBase {
+
+    @Test
+    public void should_sort_dishes_according_to_calories() {
+
+        //TODO update the DishComparator to be able to sort dishes according to their calories
+        assertThat(menu).isNotEmpty();
+        assertThat(menu.get(0).getCalories()).isEqualTo(800);
+
+        menu.sort(new DishComparator());
+
+        assertThat(menu).isNotEmpty();
+        assertThat(menu.get(0).getCalories()).isEqualTo(120);
+    }
+
 
     @Test
     public void should_find_non_vegetarian_dishes() {
@@ -36,6 +51,23 @@ public class Exercise1 extends TestBase {
 
     }
 
+    @Test
+    public void should_find_non_vegetarian_dishes_using_method_reference() {
+        // REWRITE the nonVegetarianDish predicate using method reference
+        Predicate<Dish> nonVegetarianDish = Dish::isVegetarian;
+        List<Dish> nonVegetarianDishes = filter(menu, nonVegetarianDish.negate());
+
+        assertThat(nonVegetarianDishes).isNotEmpty();
+        assertThat(nonVegetarianDishes).hasSize(5);
+
+    }
+
+
+    private class DishComparator implements Comparator<Dish> {
+        public int compare(Dish dish1, Dish dish2) {
+            return 0;//new Integer(dish1.getCalories()).compareTo(dish2.getCalories());
+        }
+    }
 
     private <T> List<T> filter(List<T> listToFilter, Predicate<T> conditionToBeUsed) {
         List<T> results = new ArrayList<>();
