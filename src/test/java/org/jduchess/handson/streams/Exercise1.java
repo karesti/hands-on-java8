dee69f8524747c8a4732311e734f74ea680cf5ae
amunozhernan <amunozhernan.externe@pagesjaunes.fr>
2015-06-09 15:53:58 +0200

    Add solutions and merge names

--- src/test/java/org/jduchess/handson/streams/Exercise1.java
+++ src/test/java/org/jduchess/handson/streams/Exercise1.java
@@ -1,10 +1,13 @@
 package org.jduchess.handson.streams;
 
+import org.assertj.core.util.Lists;
 import org.jduchess.handson.Dish;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +28,7 @@ public class Exercise1 extends TestBase {
 
     //map
     @Test
-    public void should_collect_all_dishes_name(){
+    public void should_collect_all_dishes_names(){
         List<String> names = menu.stream().map(Dish::getName).collect(toList());
 
         assertThat(names).isNotEmpty();
@@ -35,7 +38,7 @@ public class Exercise1 extends TestBase {
 
     //distinct
     @Test
-    public void should_find_out_distinct_types_of_dishes(){
+    public void should_find_out_how_many_distinct_types_of_dishes(){
         long count = menu.stream().map(Dish::getType).distinct().count();
 
         assertThat(count).isEqualTo(3);
@@ -45,7 +48,7 @@ public class Exercise1 extends TestBase {
     //limit
     //How would you use streams to filter the first two meat dishes?
     @Test
-    public void should_get_the_any_fish_dish(){
+    public void should_get_any_fish_dish(){
 
         Optional<Dish> anyFishDish = menu.stream().filter(d -> Dish.Type.FISH.equals(d.getType())).findAny();
 
@@ -58,16 +61,34 @@ public class Exercise1 extends TestBase {
     //map
     // to find out the length of the name of each dish
     @Test
-    public void should_find_out_legth_of_the_name_of_each_dish(){
-        List<Integer> dishNameLengths = menu.stream() .map(Dish::getName)
+    public void should_build_a_stream_from_the_legth_of_the_name_of_each_dish(){
+        List<Integer> dishNameLengths = menu.stream().map(Dish::getName)
                 .map(String::length)
                 .collect(toList());
 
+        assertThat(dishNameLengths).isNotEmpty();
+        assertThat(dishNameLengths).hasSize(menu.size());
+        for (Dish dish: menu){
+            assertThat(dishNameLengths).contains(dish.getName().length());
+        }
+
     }
 
-    //flatmap?? pags 125
 
-    //Filtering a menu using a stream to find out three high-calorie dish names -
+    //flatmap
+    @Test
+    public void should_count_the_number_of_characteres_of_any_dish(){
+        List<String> uniqueCharacters = menu.stream().map(dish -> dish.getName())
+                                                     .map(s -> s.split(""))
+                                                     .flatMap(Arrays::stream)
+                                                     .distinct()
+                                                     .collect(toList());
+
+
+        assertThat(uniqueCharacters).isNotEmpty();
+        assertThat(uniqueCharacters).hasSize(20);
+    }
+
     @Test
     public void should_find_out_three_high_calorie_dish_names(){
         List<String> highCalorieDishes = menu.stream().filter(d -> d.getCalories() > 300)
@@ -89,11 +110,20 @@ public class Exercise1 extends TestBase {
 
     //reduce
     @Test
-    public void should_count_number_of_calories_in_menu(){
+    public void should_count_number_of_calories_in_menu_using_reduce(){
         int calories = menu.stream() .map(Dish::getCalories)
                 .reduce(0, Integer::sum);
 
         assertThat(calories).isEqualTo(4200);
+    }
+
+    //Mapping to a numeric stream
+    @Test
+    public void should_calculate_the_sum_of_calories_in_the_menu_using_sum(){
+        int totalCalories = menu.stream().mapToInt(dish -> dish.getCalories()).sum();
+
+        assertThat(totalCalories).isEqualTo(4200);
+
     }
 
 
